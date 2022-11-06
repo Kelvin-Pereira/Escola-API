77ea21bd9b070c3406f5b1f769840afc582f2a62
Kelvin Pereira Biserra <42652204+Kelvin-Pereira@users.noreply.github.com>
2022-11-06 10:05:14 -0300

    mapeamento aluno

--- src/main/java/com/example/demo/api/aluno/domain/AlunoMapper.java
+++ src/main/java/com/example/demo/api/aluno/domain/AlunoMapper.java
@@ -5,6 +5,7 @@ import com.example.demo.api.aluno.domain.dto.AlunoOutputDTO;
 import com.example.demo.api.aluno.domain.entity.Aluno;
 import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
@@ -13,11 +14,13 @@ public interface AlunoMapper {
 
     AlunoMapper INSTANCE = Mappers.getMapper(AlunoMapper.class);
 
+    @Mapping(target = "turmaSigla", source = "turma.sigla")
     AlunoOutputDTO entityToDto(Aluno aluno);
 
+    @Mapping(target = "turma.id", source = "turmaId")
     Aluno dtoToEntity(AlunoInputDTO alunoInputDTO);
 
     @InheritConfiguration(name = "dtoToEntity")
-    Aluno dtoToEntity(AlunoInputDTO alunoInputDTO, @MappingTarget Aluno aluno);
+    void dtoToEntity(AlunoInputDTO alunoInputDTO, @MappingTarget Aluno aluno);
 
 }
