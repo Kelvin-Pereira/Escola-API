2d0461b2a414ce4f0c4faa248b29c96480d7ce69
Kelvin Pereira Biserra <42652204+Kelvin-Pereira@users.noreply.github.com>
2022-11-05 21:44:08 -0300

    Repository de aluno

--- src/main/java/com/example/demo/api/aluno/service/EditarService.java
+++ src/main/java/com/example/demo/api/aluno/service/EditarService.java
@@ -2,16 +2,22 @@ package com.example.demo.api.aluno.service;
 
 import com.example.demo.api.aluno.domain.dto.AlunoInputDTO;
 import com.example.demo.api.aluno.domain.dto.AlunoOutputDTO;
+import com.example.demo.api.aluno.domain.entity.Aluno;
+import com.example.demo.api.aluno.repository.AlunoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Function;
 
 @Service
+@RequiredArgsConstructor
 public class EditarService implements Function<AlunoInputDTO, AlunoOutputDTO> {
 
+    private final AlunoRepository repository;
 
     @Override
     public AlunoOutputDTO apply(AlunoInputDTO alunoInputDTO) {
+        repository.save(new Aluno());
         return null;
     }
 }
--- src/main/java/com/example/demo/api/aluno/service/SalvarService.java
+++ src/main/java/com/example/demo/api/aluno/service/SalvarService.java
@@ -2,16 +2,23 @@ package com.example.demo.api.aluno.service;
 
 import com.example.demo.api.aluno.domain.dto.AlunoInputDTO;
 import com.example.demo.api.aluno.domain.dto.AlunoOutputDTO;
+import com.example.demo.api.aluno.domain.entity.Aluno;
+import com.example.demo.api.aluno.repository.AlunoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Function;
 
 @Service
+@RequiredArgsConstructor
 public class SalvarService implements Function<AlunoInputDTO, AlunoOutputDTO> {
+
+    private final AlunoRepository repository;
 
     @Override
     public AlunoOutputDTO apply(AlunoInputDTO alunoInputDTO) {
-        return null;
+         repository.save(new Aluno());
+         return null;
     }
 
 }
